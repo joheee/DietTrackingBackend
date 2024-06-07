d159d9e3d7a18e6bc4d18d91b3a5b70c4f2044c3
johe lappy <johevin.blesstowi@binus.ac.id>
2024-06-07 16:50:10 +0700

    finalize auth endpoint

--- src/api/authentication/authentication.service.ts
+++ src/api/authentication/authentication.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  ConflictException,
+} from '@nestjs/common';
 import { LoginAuthenticationDto } from './dto/login-authentication.dto';
 import { RegisterAuthenticationDto } from './dto/register-authentication.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -8,26 +13,44 @@ export class AuthenticationService {
   constructor(private prisma: PrismaService) {}
 
   async login(loginAuthenticationDto: LoginAuthenticationDto) {
-    return await this.prisma.user.findUnique({
-      where: {
-        username: loginAuthenticationDto.username,
-        AND: {
-          password: loginAuthenticationDto.password,
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          username: loginAuthenticationDto.username,
+          AND: {
+            password: loginAuthenticationDto.password,
+          },
         },
-      },
-      select: {
-        username: true,
-        userDetail: true,
-      },
-    });
+        select: {
+          username: true,
+          userDetail: true,
+        },
+      });
+
+      if (!user) {
+        throw new NotFoundException('User not found or incorrect credentials');
+      }
+
+      return user;
+    } catch (error) {
+      throw new InternalServerErrorException(
+        `An error occurred during login: ${error.message}`,
+      );
+    }
   }
 
-  async register(loginAuthenticationDto: LoginAuthenticationDto) {
-    return await this.prisma.user.create({
-      data: {
-        username: loginAuthenticationDto.username,
-        password: loginAuthenticationDto.password,
-      },
-    });
+  async register(registerAuthenticationDto: RegisterAuthenticationDto) {
+    try {
+      return await this.prisma.user.create({
+        data: {
+          username: registerAuthenticationDto.username,
+          password: registerAuthenticationDto.password,
+        },
+      });
+    } catch (error) {
+      throw new InternalServerErrorException(
+        `An error occurred during registration: ${error.message}`,
+      );
+    }
   }
 }
