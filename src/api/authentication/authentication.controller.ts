598b7708e09d5ef08da29c0b52b4d27eed1a511c
johe lappy <johevin.blesstowi@binus.ac.id>
2024-06-07 16:43:06 +0700

    finalize auth endpoint

--- src/api/authentication/authentication.controller.ts
+++ src/api/authentication/authentication.controller.ts
@@ -1,14 +1,8 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { LoginAuthenticationDto } from './dto/login-authentication.dto';
 import { RegisterAuthenticationDto } from './dto/register-authentication.dto';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('authentication')
 @Controller('authentication')
@@ -17,12 +11,40 @@ export class AuthenticationController {
 
   @ApiOperation({ summary: 'login user' })
   @Post()
+  @ApiBody({
+    description: 'Create a new user',
+    type: LoginAuthenticationDto,
+    examples: {
+      example1: {
+        summary: 'template login user',
+        description: 'Example payload for login new user',
+        value: {
+          username: 'aaaaa',
+          password: 'aaaaa',
+        },
+      },
+    },
+  })
   create(@Body() createAuthenticationDto: LoginAuthenticationDto) {
     return this.authenticationService.login(createAuthenticationDto);
-    }
-    
+  }
+
   @ApiOperation({ summary: 'register user' })
   @Patch()
+  @ApiBody({
+    description: 'Create a new user',
+    type: LoginAuthenticationDto,
+    examples: {
+      example1: {
+        summary: 'template register user',
+        description: 'Example payload for register new user',
+        value: {
+          username: 'aaaaa',
+          password: 'aaaaa',
+        },
+      },
+    },
+  })
   update(@Body() createAuthenticationDto: LoginAuthenticationDto) {
     return this.authenticationService.register(createAuthenticationDto);
   }
